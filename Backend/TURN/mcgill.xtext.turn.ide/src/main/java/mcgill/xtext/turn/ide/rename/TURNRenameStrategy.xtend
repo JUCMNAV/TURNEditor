8f5a021652a4e7a68ae115d082d3be5a2241cd5b
Rijul Saini <rijulsaini.mait@gmail.com>
2019-02-05 07:56:34 -0500

    correcting the structure

--- Backend/TURN/mcgill.xtext.turn.ide/src/main/java/mcgill/xtext/turn/ide/rename/TURNRenameStrategy.xtend
+++
@@ -1,7 +0,0 @@
-package mcgill.xtext.turn.ide.rename
-
-import org.eclipse.xtext.ide.refactoring.IRenameStrategy2
-
-class TURNRenameStrategy extends IRenameStrategy2.DefaultImpl {
-	
-}
--- Backend/TURN/mcgill.xtext.turn.ide/src/main/xtend-gen/mcgill/xtext/turn/ide/rename/TURNRenameStrategy.java
+++
@@ -1,7 +0,0 @@
-package mcgill.xtext.turn.ide.rename;
-
-import org.eclipse.xtext.ide.refactoring.IRenameStrategy2;
-
-@SuppressWarnings("all")
-public class TURNRenameStrategy extends IRenameStrategy2.DefaultImpl {
-}
