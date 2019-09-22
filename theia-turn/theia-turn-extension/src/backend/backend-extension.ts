9f61c03614ee78601703dc6f57562c42f3e6105d
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2019-09-22 12:44:33 -0400

    Issue #6: small edits to .gitignore

--- theia-turn/theia-turn-extension/src/backend/backend-extension.ts
+++ theia-turn/theia-turn-extension/src/backend/backend-extension.ts
@@ -12,23 +12,23 @@ import { isWindows } from '@theia/core/lib/common/os'
 import { createSocketConnection } from 'vscode-ws-jsonrpc/lib/server'
 import * as net from 'net'
 
-const EXECUTABLE_NAME = isWindows ? 'turn-language-server.bat' : 'turn-language-server'
-const EXECUTABLE_PATH = resolve(join(__dirname, '..', '..', 'build', 'turn-language-server', 'bin', EXECUTABLE_NAME))
+const EXECUTABLE_NAME = isWindows ? 'turn-language-server.bat' : 'turn-language-server';
+const EXECUTABLE_PATH = resolve(join(__dirname, '..', '..', 'build', 'turn-language-server', 'bin', EXECUTABLE_NAME));
 
 function getPort(): number | undefined {
-    let arg = process.argv.filter(arg => arg.startsWith('--TURN_LSP='))[0]
+    let arg = process.argv.filter(arg => arg.startsWith('--TURN_LSP='))[0];
     if (!arg) {
-        return undefined
+        return undefined;
     } else {
-        return Number.parseInt(arg.substring('--TURN_LSP='.length), 10)
+        return Number.parseInt(arg.substring('--TURN_LSP='.length), 10);
     }
 }
 
 @injectable()
 class TURNLanguageServerContribution extends BaseLanguageServerContribution {
 
-    readonly id = 'turn'
-    readonly name = 'Turn'
+    readonly id = 'turn';
+    readonly name = 'Turn';
 
     readonly description = {
         id: 'turn',
@@ -37,26 +37,26 @@ class TURNLanguageServerContribution extends BaseLanguageServerContribution {
         fileEvents: [
             '**/*.turn'
         ]
-    }
+    };
 
     start(clientConnection: IConnection): void {
         let socketPort = getPort();
         if (socketPort) {
-            const socket = new net.Socket()
+            const socket = new net.Socket();
             const serverConnection = createSocketConnection(socket, socket, () => {
-                socket.destroy()
+                socket.destroy();
             });
-            this.forward(clientConnection, serverConnection)
-            socket.connect(socketPort)
+            this.forward(clientConnection, serverConnection);
+            socket.connect(socketPort);
         } else {
-            const args: string[] = []
-            const serverConnection = this.createProcessStreamConnection(EXECUTABLE_PATH, args)
-            this.forward(clientConnection, serverConnection)
+            const args: string[] = [];
+            const serverConnection = this.createProcessStreamConnection(EXECUTABLE_PATH, args);
+            this.forward(clientConnection, serverConnection);
         }
     }
 
 }
 
 export default new ContainerModule(bind => {
-    bind(LanguageServerContribution).to(TURNLanguageServerContribution).inSingletonScope()
-})
+    bind(LanguageServerContribution).to(TURNLanguageServerContribution).inSingletonScope();
+});
