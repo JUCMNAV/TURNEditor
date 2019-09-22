641c7c1c4812b248b3705aaabdd1745bad2f8b9e
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2019-09-22 14:51:37 -0400

    Issue #6: cleaned cache + adjusted gradle builds

--- turn-lsp/org.jucmnav.turn.ide/src/main/xtend-gen/org/jucmnav/turn/ide/rename/TurnRenameStrategy.java
+++
@@ -1,7 +0,0 @@
-package org.jucmnav.turn.ide.rename;
-
-import org.eclipse.xtext.ide.refactoring.IRenameStrategy2;
-
-@SuppressWarnings("all")
-public class TurnRenameStrategy extends IRenameStrategy2.DefaultImpl {
-}
