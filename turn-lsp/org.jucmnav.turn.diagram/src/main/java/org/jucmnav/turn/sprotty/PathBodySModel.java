8ea180e03bec2883deb12307ec913d7b0bb4c17b
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2019-11-02 13:07:19 -0400

    TUCM: fix fork generation + css attributes

--- turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/PathBodySModel.java
+++ turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/PathBodySModel.java
@@ -2,7 +2,6 @@ package org.jucmnav.turn.sprotty;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.jucmnav.turn.mapping.TurnSModelMapper;
 import org.jucmnav.turn.turn.EndpointWithConnect;
 import org.jucmnav.turn.turn.PathBody;
@@ -85,6 +84,7 @@ public class PathBodySModel implements TurnSModel {
 		return children;
 	}
 
+	//TODO: rename this method
 	private TurnSModel getURNSModel() {
 		
 		URNmodelElement endPathModelElement = null;
