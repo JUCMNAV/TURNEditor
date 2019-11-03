b7252260f71f411b90305a16a8f09f709a152446
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2019-11-03 02:58:03 -0500

    #20 #21 #22 #23 - TUCM: fixed node connection arc radius

--- turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/AndForkSModel.java
+++ turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/AndForkSModel.java
@@ -8,6 +8,7 @@ import org.jucmnav.turn.turn.AndFork;
 import org.jucmnav.turn.turn.PathBody;
 
 import io.typefox.sprotty.api.LayoutOptions;
+import io.typefox.sprotty.api.SEdge;
 import io.typefox.sprotty.api.SModelElement;
 
 public class AndForkSModel implements TurnSModel {
@@ -42,11 +43,49 @@ public class AndForkSModel implements TurnSModel {
 	@Override
 	public List<SModelElement> generateChildrenForSGraph() {
 		List<SModelElement> graphChildren = new ArrayList<>();
+		
 		for(PathBody pb : andFork.getPathbody() ) {
 			PathBodySModel pathBodySModel = new PathBodySModel(andFork, pb);
 			graphChildren.addAll(pathBodySModel.generateChildrenForSGraph());
 		}
+		
+		if(andFork.getConnectingAndBody() != null) {
+			PathBodySModel pathBodySModel = new PathBodySModel(andFork.getConnectingAndBody(), andFork.getConnectingAndBody());
+			graphChildren.addAll(pathBodySModel.generateChildrenForSGraph());
+			graphChildren.addAll(createEdgeFromForkToJoin());
+		}
+		
 		return graphChildren;
+	}
+	
+	private List<SModelElement> createEdgeFromForkToJoin(){
+		List<SModelElement> edgeElements = new ArrayList<>();
+		
+		String sourceId = Integer.toHexString(andFork.hashCode());
+		String targetId = String.format("join-%s", Integer.toHexString(andFork.getConnectingAndBody().hashCode()));
+		
+		TURNNode emptyNode = new TURNNode(n -> {
+			n.setType("turnnode:empty");
+			n.setId(String.format("empty-%s", sourceId));
+		});
+		
+		edgeElements.add(emptyNode);
+		
+		edgeElements.add(new SEdge(e -> {
+			e.setType("edge:connection");
+			e.setId(String.format("%s-to-%s", sourceId, emptyNode.getId()));
+			e.setSourceId(sourceId);
+			e.setTargetId(emptyNode.getId());
+		}));
+		
+		edgeElements.add(new SEdge(e -> {
+			e.setType("edge:connection");
+			e.setId(String.format("%s-to-%s", emptyNode.getId(), targetId));
+			e.setSourceId(emptyNode.getId());
+			e.setTargetId(targetId);
+		}));
+		
+		return edgeElements;
 	}
 
 }
