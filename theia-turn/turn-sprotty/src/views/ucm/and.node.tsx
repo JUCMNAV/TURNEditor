7809073587c452c2fdc0e0797a807c83880f9a3e
Gabetn <gabetadesse@gmail.com>
2019-11-02 21:19:20 -0400

    #21 TUCM: added andFork, adjusted width

--- theia-turn/turn-sprotty/src/views/ucm/and.node.tsx
+++ theia-turn/turn-sprotty/src/views/ucm/and.node.tsx
@@ -1,7 +1,7 @@
 /** @jsx svg */
 
 import { svg } from 'snabbdom-jsx';
-import {IView, RenderingContext} from "sprotty/lib";
+import {IView, RenderingContext, setAttr} from "sprotty/lib";
 import {TURNNode} from "../../models";
 import { VNode } from "snabbdom/vnode";
 
@@ -12,13 +12,11 @@ export class AndNodeView implements IView {
 
     // TODO: implement dynamic sizing based on # of children
     render(node: TURNNode, context: RenderingContext): VNode {
-        const vnode = <g class-sprotty-node={true}>
-            <rect class-body={true} class-selected={node.selected}
-                  x={0} y={0}
-                  width={node.getDimension() / 2} height={node.getDimension()}
-                  fill="none"/>
+        const vnode = <g class-ucm-node={true}>
+            <rect x={0} y={0} width={node.getDimension() / 4} height={node.getDimension()}/>
             {context.renderChildren(node)}
         </g>;
+        setAttr(vnode, 'class', node.cssClass);
         return vnode;
     }
 }
--- turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/AndForkSModel.java
+++ turn-lsp/org.jucmnav.turn.diagram/src/main/java/org/jucmnav/turn/sprotty/AndForkSModel.java
@@ -1,9 +1,11 @@
 package org.jucmnav.turn.sprotty;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jucmnav.turn.diagram.TURNNode;
 import org.jucmnav.turn.turn.AndFork;
+import org.jucmnav.turn.turn.PathBody;
 
 import io.typefox.sprotty.api.LayoutOptions;
 import io.typefox.sprotty.api.SModelElement;
@@ -20,7 +22,7 @@ public class AndForkSModel implements TurnSModel {
 	public SModelElement generate() {
 		return new TURNNode(fork ->{
 			fork.setType(TYPE);
-			fork.setId(andFork.getLongName().getLongname());//TODO: is this really what we want?
+			fork.setId(Integer.toHexString(andFork.hashCode()));
 			fork.setLayoutOptions(getLayoutOptions());
 			fork.setChildren(generateChildren());
 		});
@@ -39,8 +41,12 @@ public class AndForkSModel implements TurnSModel {
 	}
 	@Override
 	public List<SModelElement> generateChildrenForSGraph() {
-		// TODO Auto-generated method stub
-		return null;
+		List<SModelElement> graphChildren = new ArrayList<>();
+		for(PathBody pb : andFork.getPathbody() ) {
+			PathBodySModel pathBodySModel = new PathBodySModel(andFork, pb);
+			graphChildren.addAll(pathBodySModel.generateChildrenForSGraph());
+		}
+		return graphChildren;
 	}
 
 }
