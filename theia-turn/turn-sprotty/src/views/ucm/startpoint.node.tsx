facf5d07670090a3934c0e377a85f2a025207dc4
laurentpd <laurent.poulin-dube@mail.mcgill.ca>
2019-11-17 19:15:10 -0500

    update view

--- theia-turn/turn-sprotty/src/views/ucm/startpoint.node.tsx
+++ theia-turn/turn-sprotty/src/views/ucm/startpoint.node.tsx
@@ -2,18 +2,20 @@
 
 import { svg } from 'snabbdom-jsx';
 import {IView, RenderingContext} from "sprotty/lib";
-import {MAPStartNode} from "../../models";
+import {TURNNode} from "../../models";
 import { VNode } from "snabbdom/vnode";
 
 export class StartPointNodeView implements IView {
 
     static readonly TYPE = 'turnnode:startpoint';
 
-    render(node: MAPStartNode, context: RenderingContext): VNode {
-        const radius = node.getDimension() / 2;
+    render(node: TURNNode, context: RenderingContext): VNode {
+        const xOffset = Math.max(node.bounds.width, 0);
+        const yOffset = Math.max(node.bounds.height / 2, 0);
         const vnode = <g class-ucm-node={true} >
             {context.renderChildren(node)}
-            <circle cx={radius} cy={radius} r={radius} />
+            <circle class-body={true} class-selected={node.selected} cx={25} cy={yOffset} r={25} />
+            <line x1={50} x2={xOffset} y1={yOffset} y2={yOffset} />
         </g>;
         return vnode;
     }
