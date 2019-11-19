8008bf81c4b3837d5346cf0ff8b697d89d19b905
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2019-11-19 10:40:29 -0500

    TUCM: fixed curving for 6 point routes

--- theia-turn/turn-sprotty/src/views/ucm/connection.edge.tsx
+++ theia-turn/turn-sprotty/src/views/ucm/connection.edge.tsx
@@ -32,32 +32,132 @@ export class ConnectionEdgeView implements IView {
             const p3 = segments[3];
             const midLength = Math.abs(p1.y - p2.y);
             const c = Math.min(midLength / 2.0, p1.x - p0.x, p3.x - p1.x);
-            if (p3.y < p0.y) {
-                path += ` L ${p1.x - c} ${p1.y}`;
-                path += ` A ${c} ${c} 0 0 0 ${p1.x} ${p1.y - c}`;
-                path += ` L ${p2.x} ${p2.y + c}`;
-                path += ` A ${c} ${c} 0 0 1 ${p2.x + c} ${p2.y}`;
-                path += ` L ${p3.x} ${p3.y}`;
+            if (this.isGoingRight(p0, p1)) {
+                path += this.getRightPath(c, p1);
+                if (this.isGoingUp(p1, p2)) {
+                    path += this.getArcPath(c, 0, p1.x, p1.y - c); // RIGHT UP
+                    path += this.getUpPath(c, p2);
+                    if (this.isGoingRight(p2, p3)) {
+                        path += this.getArcPath(c, 1, p2.x + c, p2.y); // UP RIGHT
+                    } else {
+                        path += this.getArcPath(c, 0, p2.x - c, p2.y); // UP LEFT
+                    }
+                } else {
+                    path += this.getArcPath(c, 1, p1.x, p1.y + c); // RIGHT DOWN
+                    path += this.getDownPath(c, p2);
+                    if (this.isGoingRight(p2, p3)) {
+                        path += this.getArcPath(c, 0, p2.x + c, p2.y); // DOWN RIGHT
+                    } else {
+                        path += this.getArcPath(c, 1, p2.x - c, p2.y); // DOWN LEFT
+                    }
+                }
             } else {
-                path += ` L ${p1.x - c} ${p1.y}`;
-                path += ` A ${c} ${c} 0 0 1 ${p1.x} ${p1.y + c}`;
-                path += ` L ${p2.x} ${p2.y - c}`;
-                path += ` A ${c} ${c} 0 0 0 ${p2.x + c} ${p2.y}`;
-                path += ` L ${p3.x} ${p3.y}`;
+                // TODO
             }
-        } else {
-            // const arcRadius = this.determineArcRadius(segments);
-            // for (let p = 0; p < segments.length - 2; p++) {
-            //     const direction = this.determineArcDirection(segments[p], segments[p + 1], segments[p + 2]);
-            //     const linePoint = this.determineLinePoint(segments[p], segments[p + 1], arcRadius);
-            //     const arcStopPoint = this.determineArcStopPoint(segments[p + 1], segments[p + 2], arcRadius);
-            //     path += ` L ${linePoint.x} ${linePoint.y}`;
-            //     path += ` A ${arcRadius} ${arcRadius} 0 0 ${direction} ${arcStopPoint.x} ${arcStopPoint.y}`;
-            // }
-            // path += ` L ${segments[segments.length - 1].x} ${segments[segments.length - 1].y}`;
-            for (let p = 1; p < segments.length; p++) {
-                path += ` L ${segments[p].x} ${segments[p].y}`;
+            path += this.getLinePath(p3.x, p3.y);
+        } else if (segments.length > 4 && segments.length <= 6) {
+            const p2 = segments[2];
+            const p3 = segments[3];
+            const p4 = segments[4];
+            const p5 = segments[5];
+            const midLength = Math.min(Math.abs(p1.y - p2.y), Math.abs(p3.y - p4.y));
+            const c = Math.min(midLength / 2.0, Math.abs(p1.x - p0.x), Math.abs(p3.x - p2.x), Math.abs(p5.x - p4.x));
+            if (this.isGoingRight(p0, p1)) {
+                path += this.getRightPath(c, p1);
+                if (this.isGoingUp(p1, p2)) {
+                    path += this.getArcPath(c, 0, p1.x, p1.y - c); // RIGHT UP
+                    path += this.getUpPath(c, p2);
+                    if (this.isGoingRight(p2, p3)) {
+                        path += this.getArcPath(c, 1, p2.x + c, p2.y); // UP RIGHT
+                        path += this.getRightPath(c, p3);
+                        if (this.isGoingUp(p3, p4)) {
+                            path += this.getArcPath(c, 0, p3.x, p3.y - c); // RIGHT UP
+                            path += this.getUpPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 1, p4.x + c, p4.y); // UP RIGHT
+                            } else {
+                                path += this.getArcPath(c, 0, p4.x - c, p4.y); // UP LEFT
+                            }
+                        } else {
+                            path += this.getArcPath(c, 1, p3.x, p3.y + c); // RIGHT DOWN
+                            path += this.getDownPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 0, p4.x + c, p4.y); // DOWN RIGHT
+                            } else {
+                                path += this.getArcPath(c, 1, p4.x - c, p4.y); // DOWN LEFT
+                            }
+                        }
+                    } else {
+                        path += this.getArcPath(c, 0, p2.x - c, p2.y); // UP LEFT
+                        path += this.getLeftPath(c, p3);
+                        if (this.isGoingUp(p3, p4)) {
+                            path += this.getArcPath(c, 1, p3.x, p3.y - c); // LEFT UP
+                            path += this.getUpPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 1, p4.x + c, p4.y); // UP RIGHT
+                            } else {
+                                path += this.getArcPath(c, 0, p4.x - c, p4.y); // UP LEFT
+                            }
+                        } else {
+                            path += this.getArcPath(c, 0, p3.x, p3.y + c); // LEFT DOWN
+                            path += this.getDownPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 0, p4.x + c, p4.y); // DOWN RIGHT
+                            } else {
+                                path += this.getArcPath(c, 1, p4.x - c, p4.y); // DOWN LEFT
+                            }
+                        }
+                    }
+                } else { // RIGHT, DOWN
+                    path += this.getArcPath(c, 1, p1.x, p1.y + c); // RIGHT DOWN
+                    path += this.getDownPath(c, p2);
+                    if (this.isGoingRight(p2, p3)) {
+                        path += this.getArcPath(c, 0, p2.x + c, p2.y); // DOWN RIGHT
+                        path += this.getRightPath(c, p3);
+                        if (this.isGoingUp(p3, p4)) {
+                            path += this.getArcPath(c, 0, p3.x, p3.y - c); // RIGHT UP
+                            path += this.getUpPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 1, p4.x + c, p4.y); // UP RIGHT
+                            } else {
+                                path += this.getArcPath(c, 0, p4.x - c, p4.y); // UP LEFT
+                            }
+                        } else {
+                            path += this.getArcPath(c, 1, p3.x, p3.y + c); // RIGHT DOWN
+                            path += this.getDownPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 0, p4.x + c, p4.y); // DOWN RIGHT
+                            } else {
+                                path += this.getArcPath(c, 1, p4.x - c, p4.y); // DOWN LEFT
+                            }
+                        }
+                    } else { // RIGHT, DOWN, LEFT
+                        path += this.getArcPath(c, 1, p2.x - c, p2.y); // DOWN LEFT
+                        path += this.getLeftPath(c, p3);
+                        if (this.isGoingUp(p3, p4)) {
+                            path += this.getArcPath(c, 1, p3.x, p3.y - c); // LEFT UP
+                            path += this.getUpPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 1, p4.x + c, p4.y); // UP RIGHT
+                            } else {
+                                path += this.getArcPath(c, 0, p4.x - c, p4.y); // UP LEFT
+                            }
+                        } else {
+                            path += this.getArcPath(c, 0, p3.x, p3.y + c); // LEFT DOWN
+                            path += this.getDownPath(c, p4);
+                            if (this.isGoingRight(p4, p5)) {
+                                path += this.getArcPath(c, 0, p4.x + c, p4.y); // DOWN RIGHT
+                            } else {
+                                path += this.getArcPath(c, 1, p4.x - c, p4.y); // DOWN LEFT
+                            }
+                        }
+                    }
+                }
+            } else { // LEFT
+                path += ` L ${p1.x + c} ${p1.y}`;
+                // TODO
             }
+            path += this.getLinePath(p5.x, p5.y);
         }
 
         return <path d={path}/>;
@@ -67,90 +167,36 @@ export class ConnectionEdgeView implements IView {
         return <text class-sprotty-edge-dangling={true}>{message}</text>;
     }
 
-    protected determineArcRadius(points: Point[]): number {
-        let min = Number.MAX_VALUE;
-        for (let i = 0; i < points.length - 1; i++) {
-            if (points[i].x !== points[i + 1].x) {
-                const xDifference = Math.abs(points[i].x - points[i + 1].x);
-                min =  xDifference < min ? xDifference : min;
-            } else if (points[i].y !== points[i + 1].y) {
-                const yDifference = Math.abs(points[i].y - points[i + 1].y) / 2.0;
-                min = yDifference < min ? yDifference : min;
-            }
-        }
-        return min;
+    protected isGoingUp(firstPoint: Point, secondPoint: Point): boolean {
+        return firstPoint.y > secondPoint.y;
     }
 
-    protected determineArcDirection(p1: Point, p2: Point, p3: Point): number {
-        const xDirection = p3.x - p1.x;
-        const yDirection = p1.y - p3.y;
-        const isHorizontalBeforeVertical = p2.x - p1.x > 0;
-        let arc: number = 0;
-        if (isHorizontalBeforeVertical) {
-            if (yDirection > 0 && xDirection > 0) {
-                arc = 0;
-            } else if (yDirection < 0 && xDirection > 0) {
-                arc = 1;
-            } else if (yDirection > 0 && xDirection < 0) {
-                arc = 1;
-            } else if (yDirection < 0 && xDirection < 0) {
-                arc = 0;
-            }
-        } else {
-            if (yDirection > 0 && xDirection > 0) {
-                arc = 1;
-            } else if (yDirection < 0 && xDirection > 0) {
-                arc = 0;
-            } else if (yDirection > 0 && xDirection < 0) {
-                arc = 0;
-            } else if (yDirection < 0 && xDirection < 0) {
-                arc = 1;
-            }
-        }
-        return arc;
+    protected isGoingRight(firstPoint: Point, secondPoint: Point): boolean {
+        return secondPoint.x > firstPoint.x;
     }
 
-    protected determineArcStopPoint(p2: Point, p3: Point, arcRadius: number): Point {
-        const xDirection = p3.x - p2.x;
-        const yDirection = p2.y - p3.y;
-        let x = p3.x;
-        let y = p3.y;
-        if (xDirection > 0) {
-            x = p2.x + arcRadius;
-        } else if (xDirection < 0) {
-            x = p2.x - arcRadius;
-        } else {
-            if (yDirection > 0) {
-                y = p2.y + arcRadius;
-            } else if (yDirection < 0) {
-                y = p2.y - arcRadius;
-            }
-        }
-        return new class implements Point {
-            readonly x: number = x;
-            readonly y: number = y;
-        };
+    protected getDownPath(arcRadius: number, p: Point): String {
+        return this.getLinePath(p.x, p.y - arcRadius);
     }
 
-    protected determineLinePoint(p1: Point, p2: Point, arcRadius: number): Point {
-        const xDirection = p2.x - p1.x;
-        const yDirection = p1.y - p2.y;
-        let x = p2.x;
-        let y = p2.y;
-        if (xDirection > 0) {
-            x = p2.x - arcRadius;
-        } else if (xDirection < 0) {
-            x = p1.x + arcRadius;
-        } else {
-            if (yDirection > 0) {
-                y = p2.y + arcRadius;
-            } else if (yDirection < 0) {
-                y = p1.y - arcRadius;
-            }
-        }
-        return new class implements Point {
-            readonly x: number = x;
-            readonly y: number = y;
-        };
+    protected getUpPath(arcRadius: number, p: Point): String {
+        return this.getLinePath(p.x, p.y + arcRadius);
     }
+
+    protected getLeftPath(arcRadius: number, p: Point): String {
+        return this.getLinePath(p.x + arcRadius, p.y);
+    }
+
+    protected getRightPath(arcRadius: number, p: Point): String {
+        return this.getLinePath(p.x - arcRadius, p.y);
+    }
+
+    protected getArcPath(arcRadius: number, clockwise: number, stopX: number, stopY: number): String {
+        return ` A ${arcRadius} ${arcRadius} 0 0 ${clockwise} ${stopX} ${stopY}`;
+    }
+
+    protected getLinePath(stopX: number, stopY: number): String {
+        return ` L ${stopX} ${stopY}`;
+    }
+
 }
